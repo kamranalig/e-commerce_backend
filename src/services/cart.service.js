c5f7829b88961eb652afdd71a21ef070d3ecba04
kamran ali <km775.ali@gmail.com>
2024-03-20 06:09:48 +0500

    cart service created

---
+++ src/services/cart.service.js
@@ -0,0 +1,11 @@
+const Cart = require("../models/cart.model");
+async function createCart(user) {
+  try {
+    const cart = new Cart({ user });
+    const createdCart = await cart.save();
+    return createCart;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+module.exports = { createCart };
