6465e91d5bbb169c41fe444495497e4bce0a8086
kamran ali <km775.ali@gmail.com>
2024-03-20 08:18:14 +0500

    cart service completed

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,11 +1,65 @@
 const Cart = require("../models/cart.model");
+const CartItem = require("../models/cartItem.model");
+const Product = require("../models/product.model");
 async function createCart(user) {
   try {
     const cart = new Cart({ user });
     const createdCart = await cart.save();
-    return createCart;
+    return createdCart;
   } catch (error) {
     throw new Error(error.message);
   }
 }
-module.exports = { createCart };
+
+async function findUserCart(user) {
+  try {
+    let cart = await Cart.findOne({ user: user });
+    let cartItems = await CartItem.find({ cart: cart._id }).populate("product");
+    cart.cartItems = cartItems;
+    let totalPrice = 0;
+    let totalDiscountedPrice = 0;
+    let totalItem = 0;
+
+    for (let cartItem of cart.cartItems) {
+      totalPrice += cartItem.price;
+      totalDiscountedPrice += cartItem.discountedPrice;
+      totalItem += cartItem.quantity;
+    }
+
+    cart.totalPrice = totalPrice;
+    cart.totalItem = totalItem;
+    cart.discounte = totalPrice - totalDiscountedPrice;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function addCartItem(userId, req) {
+  try {
+    const cart = await Cart.findOne({ user: userId });
+    const product = await Product.findById(req.productId);
+    const isPresent = await CartItem.findOne({
+      cart: cart._id,
+      product: product._id,
+      userId,
+    });
+    if (!isPresent) {
+      const cartItem = new CartItem({
+        product: product._id,
+        cart: cart._id,
+        quantity: 1,
+        userId,
+        price: product.price,
+        size: req.size,
+        discountedPrice: product.discountedPrice,
+      });
+      const createdCartItem = await cartItem.save();
+      cart.cartItems.push(createdCartItem);
+      await cart.save();
+      return "Item Added to Cart";
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+module.exports = { createCart, findUserCart, addCartItem };
