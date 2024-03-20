7bebd8d3d1142d480ea400d0a0f95a385f597647
kamran ali <km775.ali@gmail.com>
2024-03-20 06:11:34 +0500

    cart logic implenetes

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,12 +1,13 @@
 const userService = require("../services/user.service");
 const jwtProvider = require("../config/jwtProvider");
 const bcrypt = require("bcrypt");
+const cartService = require("../services/cart.service");
 const register = async (req, res) => {
   try {
     const user = await userService.createUser(req.body);
     const jwt = jwtProvider.generateToken(user._id);
 
-    // await cartService.createCart(user);
+    await cartService.createCart(user);
 
     return res.status(200).send({ jwt, message: "register success" });
   } catch (error) {
