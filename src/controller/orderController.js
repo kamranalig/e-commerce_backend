c04a46e5c75625bebfa9b0e007596d7205dcc5a2
kamran ali <km775.ali@gmail.com>
2024-03-20 18:12:05 +0500

    order controller added

---
+++ src/controller/orderController.js
@@ -0,0 +1,36 @@
+const orderService = require("../services/order.service");
+const createOrders = async (req, res) => {
+  const user = req.user;
+  try {
+    const createOrder = await orderService.createOrder(user, req.body);
+    return res.status(201).send(createOrder);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const findOrderById = async (req, res) => {
+  const user = req.user;
+  try {
+    let createdOrder = await orderService.findOrderById(req.params.Id);
+    return res.status(201).send(createdOrder);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const orderHistory = async (req, res) => {
+  const user = req.user;
+  try {
+    let createdOrder = await orderService.userOrderHistory(user._id);
+    return res.status(201).send(createdOrder);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+module.exports = {
+  createOrders,
+  findOrderById,
+  orderHistory,
+};
